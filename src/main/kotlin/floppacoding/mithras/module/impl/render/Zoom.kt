fd286c00ffc706568e7e7325c67030648691b9e3
AtonRay <107542447+AtonRay@users.noreply.github.com>
2023-10-31 21:42:06 +0100

    Changed the zoom module so that it now has to be enabled for the keybind to work

--- src/main/kotlin/floppacoding/mithras/module/impl/render/Zoom.kt
+++ src/main/kotlin/floppacoding/mithras/module/impl/render/Zoom.kt
@@ -3,6 +3,7 @@ package floppacoding.mithras.module.impl.render
 import floppacoding.mithras.Mithras
 import floppacoding.mithras.events.InputEvent
 import floppacoding.mithras.events.MouseScrollEvent
+import floppacoding.mithras.events.WorldChangeEvent
 import floppacoding.mithras.module.Category
 import floppacoding.mithras.module.Module
 import floppacoding.mithras.module.settings.Visibility
@@ -37,6 +38,7 @@ object Zoom : Module(
      * Used as a stand in for the games fov setting when zoomed in.
      */
     private val fakeFov = JavaExtensions.provideFakeFov(MIN_FOV, BASE_FOV)
+    private var isZoomed = false
 
 
     /**
@@ -45,36 +47,19 @@ object Zoom : Module(
      * @see floppacoding.mithras.mixin.GameOptionsMixin.onGetFov
      */
     fun fovOverride(): SimpleOption<Int>? {
-        return if (this.enabled) {
+        return if (this.enabled && isZoomed) {
             fakeFov
         }else
             null
     }
 
     /**
-     * Prevent the message from showing.
+     * Prevent the keybind from toggling the module.
      */
-    override fun onKeyBind() {
-        toggle()
-    }
-
-    override fun onEnable() {
-        if (Mithras.mc.currentScreen != null){
-            // Prevents this from being activated in a gui
-            toggle()
-            return
-        }
-        fakeFov.value = zoomState
-        if (smoothCamera) {
-            wasSmoothCameraEnabled = Mithras.mc.options.smoothCameraEnabled
-            Mithras.mc.options.smoothCameraEnabled = true
-        }
-        super.onEnable()
-    }
+    override fun onKeyBind() {}
 
     override fun onDisable() {
-        Mithras.mc.options.smoothCameraEnabled = wasSmoothCameraEnabled
-        Mithras.moduleConfig.saveConfig()
+        isZoomed = false
         super.onDisable()
     }
 
@@ -83,18 +68,39 @@ object Zoom : Module(
      */
     @EventHandler
     fun onInput(event: InputEvent) {
-        if(event.key == this.keyBind && event.action == InputEvent.RELEASED) {
-            toggle()
+        if(event.key == this.keyBind) {
+            when(event.action) {
+                InputEvent.RELEASED -> {
+                    isZoomed = false
+                    Mithras.mc.options.smoothCameraEnabled = wasSmoothCameraEnabled
+                    Mithras.moduleConfig.saveConfig()
+                }
+                InputEvent.PRESSED -> {
+                    // Prevents this from being activated in a gui
+                    if (Mithras.mc.currentScreen != null) return
+                    isZoomed = true
+                    fakeFov.value = zoomState
+                    if (smoothCamera) {
+                        wasSmoothCameraEnabled = Mithras.mc.options.smoothCameraEnabled
+                        Mithras.mc.options.smoothCameraEnabled = true
+                    }
+                }
+            }
         }
     }
 
     @EventHandler
     fun onScroll(event: MouseScrollEvent) {
-        if (!scrollToZoom) return
+        if (!isZoomed || !scrollToZoom) return
         // Linearized discrete approximation of a 1/x zooming behaviour.
         val delta = -(((zoomState * zoomState).toDouble() / BASE_FOV  * 0.3).coerceAtLeast(1.0) * event.amount).toInt()
         zoomState += delta // The Number setting takes care of coercing the value in the allowed range.
         fakeFov.value = zoomState
         event.cancel()
+    }
+
+    @EventHandler
+    fun onWarp(event: WorldChangeEvent) {
+        isZoomed = false
     }
 }
